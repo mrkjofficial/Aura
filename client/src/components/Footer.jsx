4ec9afdbfe9ebec23a538bee18af89ce0e154b53
Karan Jaiswal <karanjaiswal0000@gmail.com>
2023-01-13 17:18:33 +0530

    Commit @ 13-01-2023

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,142 @@
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import MailIcon from "@mui/icons-material/Mail";
+import PhoneIcon from "@mui/icons-material/Phone";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import RoomIcon from "@mui/icons-material/Room";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+import payments from "../assets/payments.png";
+
+const Container = styled.div`
+	display: flex;
+	${mobile({ flexDirection: "column" })}
+`;
+
+const Left = styled.div`
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+	margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+	display: flex;
+`;
+
+const SocialIcon = styled.div`
+	width: 40px;
+	height: 40px;
+	border-radius: 50%;
+	color: white;
+	background-color: #${(props) => props.color};
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-right: 20px;
+`;
+
+const Center = styled.div`
+	flex: 1;
+	padding: 20px;
+	${mobile({ display: "none" })}
+`;
+
+const Title = styled.h3`
+	margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+	margin: 0;
+	padding: 0;
+	list-style: none;
+	display: flex;
+	flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+	width: 50%;
+	margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+	flex: 1;
+	padding: 20px;
+	${mobile({ backgroundColor: "#fff8f8" })}
+`;
+
+const ContactItem = styled.div`
+	margin-bottom: 20px;
+	display: flex;
+	align-items: center;
+`;
+
+const Payment = styled.img`
+	width: 50%;
+`;
+
+const Footer = () => {
+	return (
+		<Container>
+			<Left>
+				<Logo>AURA</Logo>
+				<Desc>
+					There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+					alteration in some form, by injected humour, or randomised words which don’t look even slightly
+					believable.
+				</Desc>
+				<SocialContainer>
+					<SocialIcon color="3B5999">
+						<FacebookIcon />
+					</SocialIcon>
+					<SocialIcon color="E4405F">
+						<InstagramIcon />
+					</SocialIcon>
+					<SocialIcon color="E60023">
+						<PinterestIcon />
+					</SocialIcon>
+					<SocialIcon color="55ACEE">
+						<TwitterIcon />
+					</SocialIcon>
+				</SocialContainer>
+			</Left>
+			<Center>
+				<Title>Useful Links</Title>
+				<List>
+					<ListItem>Home</ListItem>
+					<ListItem>Cart</ListItem>
+					<ListItem>Mens Fashion</ListItem>
+					<ListItem>Womens Fashion</ListItem>
+					<ListItem>Accessories</ListItem>
+					<ListItem>My Account</ListItem>
+					<ListItem>My Order</ListItem>
+					<ListItem>Wishlist</ListItem>
+					<ListItem>Terms</ListItem>
+					<ListItem>F.A.Qs</ListItem>
+				</List>
+			</Center>
+			<Right>
+				<Title>Contact</Title>
+				<ContactItem>
+					<RoomIcon style={{ marginRight: "10px" }} /> Chinna Kalapet, Puducherry - 605014
+				</ContactItem>
+				<ContactItem>
+					<PhoneIcon style={{ marginRight: "10px" }} /> 1800-123-4567
+				</ContactItem>
+				<ContactItem>
+					<MailIcon style={{ marginRight: "10px" }} /> contact@aura.in
+				</ContactItem>
+				<Payment src={payments} />
+			</Right>
+		</Container>
+	);
+};
+
+export default Footer;
---
+++ client/src/data.js
@@ -0,0 +1,91 @@
+import slide_1 from "./assets/slide_1.png";
+import slide_2 from "./assets/slide_2.png";
+import slide_3 from "./assets/slide_3.png";
+import category_1 from "./assets/category_1.png";
+import category_2 from "./assets/category_2.png";
+import category_3 from "./assets/category_3.png";
+import popular_products_1 from "./assets/popular_products_1.png";
+import popular_products_2 from "./assets/popular_products_2.png";
+import popular_products_3 from "./assets/popular_products_3.png";
+import popular_products_4 from "./assets/popular_products_4.png";
+import popular_products_5 from "./assets/popular_products_5.png";
+import popular_products_6 from "./assets/popular_products_6.png";
+import popular_products_7 from "./assets/popular_products_7.png";
+import popular_products_8 from "./assets/popular_products_8.png";
+
+export const sliderItems = [
+	{
+		id: 1,
+		img: slide_1,
+		title: "SUMMER SALE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "f5fafd",
+	},
+	{
+		id: 2,
+		img: slide_2,
+		title: "AUTUMN COLLECTION",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "fcf1ed",
+	},
+	{
+		id: 3,
+		img: slide_3,
+		title: "LOUNGEWEAR LOVE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "fbf0f4",
+	},
+];
+
+export const categories = [
+	{
+		id: 1,
+		img: category_1,
+		title: "SHIRT STYLE!",
+	},
+	{
+		id: 2,
+		img: category_2,
+		title: "LOUNGEWEAR LOVE",
+	},
+	{
+		id: 3,
+		img: category_3,
+		title: "LIGHT JACKETS",
+	},
+];
+
+export const popularProducts = [
+	{
+		id: 1,
+		img: popular_products_1,
+	},
+	{
+		id: 2,
+		img: popular_products_2,
+	},
+	{
+		id: 3,
+		img: popular_products_3,
+	},
+	{
+		id: 4,
+		img: popular_products_4,
+	},
+	{
+		id: 5,
+		img: popular_products_5,
+	},
+	{
+		id: 6,
+		img: popular_products_6,
+	},
+	{
+		id: 7,
+		img: popular_products_7,
+	},
+	{
+		id: 8,
+		img: popular_products_8,
+	},
+];
