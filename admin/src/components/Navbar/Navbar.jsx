07d395cfb0b4c3175e5174b5a62857e53f01f8fa
Karan Jaiswal <karanjaiswal0000@gmail.com>
2023-02-04 15:02:19 +0530

    Commit @ 04-02-2023

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import "./Navbar.scss";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LanguageIcon from "@mui/icons-material/Language";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className="navbar">
+			<div className="navbar__wrapper">
+				<div className="navbar--left">
+					<span className="logo">AURA</span>
+				</div>
+				<div className="navbar--right">
+					<div className="icon-container">
+						<NotificationsNoneIcon />
+						<span className="icon-badge">2</span>
+					</div>
+					<div className="icon-container">
+						<LanguageIcon />
+					</div>
+					<div className="icon-container">
+						<SettingsIcon />
+					</div>
+					<img
+						src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg"
+						alt="user-avatar"
+						className="user-avatar"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
